03e3d2b5259922b7d8e542eaa0f326c1dfb838ca
gabriel2171 <Gabriel2171>
2023-01-12 15:21:49 +0100

    6 eme

--- page_Pour_ajouter.php
+++ page_Pour_ajouter.php
@@ -1,18 +1,36 @@
-
-<!DOCTYPE html>
 <html lang="fr">
-  <head>
+<head>
+    <title> </title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Page poiur ajouter </title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="style.css
+    <meta name="viewport" content="width=device-width, initial-scale-1">
+</head>
+<body>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-10"><?php include "entete.php"?></div>
+        <div class="col-md-10"><?php include "entete.php"?></div><!--j'inporte entete.php dans mon accueil -->
         <div class="col-md-2"><img src="image/logo.jpg"height="80" class="img-thumbnail" alt="logo "></div>
     </div>
+    <div class="row">
+    <div class="col-md-5">
+        </form>
+        Ajouter Membre
+            <?php
+                require_once('connexion.php');
+                $stmt1 = $connexion->prepare("SELECT * FROM utilisateur");// on selectionne tout les auteurs
+                $stmt1->setFetchMode(PDO::FETCH_OBJ);
+                $stmt1->execute();
+            ?>
+        <form action="ajouter_membre.php" method="post">
+        nom: <input type="text" placeholder="(saisir le nom )"class="form-control" name="nom" /><br>
+        prenom: <input type="text" placeholder="(saisir le prenom )"class="form-control" name="prenom" /><br>
+        mel: <input type="text" placeholder="(saisir le mel )"class="form-control" name="mel" /><br>
+        ville : <input type="text" placeholder="(saisir le ville )"class="form-control" name="ville" /><br>
+        codepostal: <input type="text"placeholder="(saisir le codepostal)"class="form-control" name="codepostal" /><br>
+        adresse: <input type="text"placeholder="(saisir l' adresse )"class="form-control" name="adresse" /><br>
+        <input type="submit" />
 </div>
-
-    <div class="col-md-3"><div class="border border-sucess p-3 mb-3"><?php include "formulaire.php"?></div></div>
-    <div class="col-md-3"><?php include "ajouter_livre.php"?></div>
+</div>
+</div>
+</form>
+</body>
+</html>
